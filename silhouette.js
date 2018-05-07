b470acebe3490969b4621824d402e06d3eb9010b
Khang Hoang <hoangtrieukhang@gmail.com>
2018-05-06 18:30:54 -0700

    clear all the module cached dependencies

--- silhouette.js
+++ silhouette.js
@@ -3,30 +3,54 @@ const path = require('path');
 
 const { start } = require('./hot');
 
+let cachedModules = {};
+
 module.exports = (config) => {
-  const { module, route, directory } = config[Object.keys(config)[0]];
+  const { name: module, route, directory, method } = config;
 
   const finalModulePath = module.startsWith('.') ?
     path.join(process.cwd(), module)
     : module;
 
-  const app = express();
+  const newAppFactory = method ? require(finalModulePath)[method] : require(finalModulePath);
+  const newAppArgument = config.arguments.length > 0 ? config.arguments[0] : {}
+  const app = newAppFactory(newAppArgument);
+
   if (route) {
     app.use(route, (req, res, next) => {
-      const newApp = require(finalModulePath)();
+      const newAppFactory = method ? require(finalModulePath)[method] : require(finalModulePath);
+      const newAppArgument = config.arguments.length > 0 ? config.arguments[0] : {}
+      const newApp = newAppFactory(newAppArgument);
       newApp(req, res, next);
     });
   } else {
     app.use((req, res, next) => {
-      const newApp = require(finalModulePath)();
+      const newAppFactory = method ? require(finalModulePath)[method] : require(finalModulePath);
+      const newAppArgument = config.arguments.length > 0 ? config.arguments[0] : {}
+      const newApp = newAppFactory(newAppArgument);
       newApp(req, res, next);
     });
   }
 
   start(directory, () => {
-    delete require.cache[require.resolve(finalModulePath)];
-  });
+    const cachedModule = require.cache[require.resolve(finalModulePath)];
+    cachedModules[require.resolve(finalModulePath)] = cachedModule;
+    clearCachedChildrenModulesOfModule(cachedModule);
+    Object.keys(cachedModules).map(moduleName => {
+      delete require.cache[require.resolve(moduleName)];
+    });
+    cachedModules = {};
+  }); 
 
   return app;
 };
 
+const clearCachedChildrenModulesOfModule = (module) => {
+  if (module && module.children) {
+    module.children.forEach((mol) => {
+      if (cachedModules[mol.id]) return;
+      cachedModules[require.resolve(mol.id)] = mol;
+      clearCachedChildrenModulesOfModule(mol);
+    });
+  }
+};
