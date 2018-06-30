0970c822763d77abaae85f270ec5273773e80989
Khang Hoang <hoangtrieukhang@gmail.com>
2018-06-29 20:45:00 -0700

    debounce reload callbacks

--- hot.js
+++ hot.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
 
 const start = (dir, fn) => {
   const watcher = chokidar.watch(dir);
+  const hotReloadFn = () => {
+    console.log(`Hot-Reload ${ dir }`);
+    fn();
+  }
   watcher.on('ready', () => {
-    watcher.on('all', () => {
-      console.log(`Hot-Reload ${ dir }`);
-      fn();
-    });
+    watcher.on('all', debounce(hotReloadFn, 1000));
   });
 }
 
